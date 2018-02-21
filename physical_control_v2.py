cd1cb74b8c1a8526966f1eb790754407742a4eec
ninglesby <ninglesby@animalstudio.com>
2018-02-21 10:18:31 -0500

    added logarithmic and exponential interpolation for the translate function

--- physical_control_v2.py
+++ physical_control_v2.py
@@ -16,21 +16,35 @@ import config
 
 def translate(value, map_from_min, map_from_max, map_to_min, map_to_max, mode="LINEAR"):
     
+
+    # Figure out how 'wide' each range is
+    from_span = map_from_max - map_from_min
+    to_span = map_to_max - map_to_min
+
+    # Convert the left range into a 0-1 range (float)
+    value_scaled = float(value - map_from_min) / float(from_span)
+    
+
     if mode == "LINEAR":
-
-        # Figure out how 'wide' each range is
-        from_span = map_from_max - map_from_min
-        to_span = map_to_min - map_from_min
-
-        # Convert the left range into a 0-1 range (float)
-        value_scaled = float(value - map_from_min) / float(from_span)
 
         # Convert the 0-1 range into a value in the right range.
         return map_to_min + (value_scaled * to_span)
 
     elif mode == "LOG":
 
-        return "LOG mode is not ready yet"
+        if value_scaled < .01:
+            
+            return 0
+
+        log = ((math.log(value_scaled, 10))+2)/2
+
+        return map_to_min + (log * to_span)
+
+    elif mode == "EXP":
+
+        exp = math.pow(value_scaled, 2)
+
+        return map_to_min + (exp * to_span)
 
 
 
--- sandy_box.py
+++ sandy_box.py
@@ -1,11 +1,36 @@
-import pigpio
-import config
+import math
+
+def translate(value, map_from_min, map_from_max, map_to_min, map_to_max, mode="LINEAR"):
+    
+
+    # Figure out how 'wide' each range is
+    from_span = map_from_max - map_from_min
+    to_span = map_to_max - map_to_min
+
+    # Convert the left range into a 0-1 range (float)
+    value_scaled = float(value - map_from_min) / float(from_span)
+    
+
+    if mode == "LINEAR":
+
+        # Convert the 0-1 range into a value in the right range.
+        return map_to_min + (value_scaled * to_span)
+
+    elif mode == "LOG":
+
+    	if value_scaled < .01:
+    		
+    		return 0
+
+    	log = ((math.log(value_scaled, 10))+2)/2
+
+        return map_to_min + (log * to_span)
+
+    elif mode == "EXP":
+
+        exp = math.pow(value_scaled, 2)
+
+        return map_to_min + (exp * to_span)
 
 
-pi = pigpio.pi()
-
-blue = config.GPIO_STATUS_BLUE
-
-pi.write(blue, 1)
-
-pi.stop()
+print translate(5, 0, 10, 0, 2, mode="EXP")
